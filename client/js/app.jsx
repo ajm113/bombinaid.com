576fbf648ad7f15beed5f30c7d229b491e0716a9
Andrew McRobb <andrewmcrobb@gmail.com>
2017-12-24 20:27:41 -0700

    Changed from gulp to webpack.

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -4,6 +4,12 @@ import AsyncRoute from 'preact-async-route';
 
 import Home from './components/Home.jsx';
 
+function requireAll (r) { r.keys().forEach(r); }
+
+requireAll(require.context('../pug', true, /\.pug$/));
+
+import '../scss/style.scss';
+
 const Main = () => (
     <div class="container grid-lg" id="root">
         <Router>
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, './priv');
+const APP_DIR = path.resolve(__dirname, './client');
+
+const config = {
+    entry: APP_DIR + '/js/app.jsx',
+    output: {
+    path: BUILD_DIR,
+        filename: 'assets/js/app.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR + '/js',
+                loader : 'babel-loader'
+            },
+            {
+                test: /\.pug$/,
+                include : APP_DIR + '/pug',
+                loaders: ['file-loader?name=[name].html', 'pug-html-loader?compressed&exports=false']
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
+    }
+};
+
+
+
+module.exports = config;
